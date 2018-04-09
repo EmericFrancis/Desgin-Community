17e89a0711b00955079fc13b74368469f04c7170
RichGibbons <richard.gibbons@landregistry.gov.uk>
2018-04-09 11:27:37 +0100

    changed service name
    to resolve pull request conflict

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Design community',
+  serviceName: 'Design and Content Design Community of Practice',
 
   // Default port that prototype runs on
   port: '3000',
